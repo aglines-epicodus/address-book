6f750dd2f81d3f01cf68bc1b5d98e6327f82af7a
Andrew Glines <aglines.epicodus@gmail.com>
2017-05-30 02:07:31 -0700

    Add code to create Contact class and display all users on route from root

---
+++ Objects/Contact.cs
@@ -0,0 +1,38 @@
+namespace AddressBook.Objects
+{
+  public class Contact
+  {
+    private string _name;
+    private string _phone;
+    private string _email;
+
+    public string GetName()
+    {
+      _name = name;
+    }
+    public string GetPhone()
+    {
+      _phone = phone;
+    }
+    public string GetEmail()
+    {
+      _email = email;
+    }
+
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public void SetPhone(string newPhone)
+    {
+      _phone = newPhone;
+    }
+    public void SetEmail(string newEmail)
+    {
+      _email = newEmail;
+    }
+
+
+
+  }
+}
