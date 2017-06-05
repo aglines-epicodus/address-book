abf26fbcf397cacc8b15f32fbcb782917369da60
Andrew Glines <aglines-epicodus@gmail.com>
2017-06-05 14:52:31 -0700

    Added Views/Shared code and tested in index.cshtml

--- Objects/Contact.cs
+++ Objects/Contact.cs
@@ -2,37 +2,39 @@ namespace AddressBook.Objects
 {
   public class Contact
   {
-    private string _name;
-    private string _phone;
-    private string _email;
+    public string name;
+    public string phone;
+    public string email;
+  }
 
-    public string GetName()
-    {
-      _name = name;
-    }
-    public string GetPhone()
-    {
-      _phone = phone;
-    }
-    public string GetEmail()
-    {
-      _email = email;
-    }
 
-    public void SetName(string newName)
-    {
-      _name = newName;
-    }
-    public void SetPhone(string newPhone)
-    {
-      _phone = newPhone;
-    }
-    public void SetEmail(string newEmail)
-    {
-      _email = newEmail;
-    }
+
+    // public string GetName()
+    // {
+    //   name = _name;
+    // }
+    // public string GetPhone()
+    // {
+    //   _phone = phone;
+    // }
+    // public string GetEmail()
+    // {
+    //   _email = email;
+    // }
+    //
+    // public void SetName(string newName)
+    // {
+    //   _name = newName;
+    // }
+    // public void SetPhone(string newPhone)
+    // {
+    //   _phone = newPhone;
+    // }
+    // public void SetEmail(string newEmail)
+    // {
+    //   _email = newEmail;
+    // }
 
 
 
   }
-}
